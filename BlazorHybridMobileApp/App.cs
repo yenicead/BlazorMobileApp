b54f318e49ef6d1c0fe0572138645ade02e814f2
Adem Yenice <adem.yenice@arutesolutions.com>
2020-10-01 21:48:57 +0300

    Created first mobile project with blazor

---
+++ BlazorHybridMobileApp/App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using Microsoft.MobileBlazorBindings.WebView;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BlazorHybridMobileApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            BlazorHybridHost.AddResourceAssembly(GetType().Assembly, contentRoot: "WebUI/wwwroot");
+
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Adds web-specific services such as NavigationManager
+                    services.AddBlazorHybrid();                 
+                })
+                .Build();
+
+            MainPage = new ContentPage { Title = "First Blazor Mobile Application" };
+            host.AddComponent<Main>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
